29c8bc5e7e07bb02915e7e9811c91bad08e01af8
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-10-15 13:42:52 +0200

    Fix SwiftLint warnings + Update to Xcode 10 & Swift 4.2

--- Frameworks/MungoHealer/ErrorHandler.swift
+++ Frameworks/MungoHealer/ErrorHandler.swift
@@ -1,7 +1,11 @@
-// Created by Cihat Gündüz on 15.10.18.
+//
+//  Created by Cihat Gündüz on 27.06.17.
+//  Copyright © 2017 Jamit Labs GmbH. All rights reserved.
+//
 
 import Foundation
 
-public class ErrorHandler {
-
+/// The main error handler.
+public enum ErrorHandler {
+    // TODO: not yet implemented
 }
