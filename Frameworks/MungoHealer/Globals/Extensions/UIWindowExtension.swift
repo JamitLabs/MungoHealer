d1ccbc5e7aa4ff1f75d2891cc39887230ea5a883
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-12-03 15:18:48 +0100

    Only include UIKit based code on iOS / tvOS

--- Frameworks/MungoHealer/Globals/Extensions/UIWindowExtension.swift
+++ Frameworks/MungoHealer/Globals/Extensions/UIWindowExtension.swift
@@ -6,16 +6,18 @@
 //  Copyright © 2018 Jamit Labs GmbH. All rights reserved.
 //
 
-import UIKit
+#if os(iOS) || os(tvOS)
+    import UIKit
 
-extension UIWindow {
-    var topViewController: UIViewController {
-        return topViewController(in: rootViewController!)
-    }
+    extension UIWindow {
+        var topViewController: UIViewController {
+            return topViewController(in: rootViewController!)
+        }
 
-    private func topViewController(in viewCtrl: UIViewController) -> UIViewController {
-        guard let presentedViewCtrl = viewCtrl.presentedViewController else { return viewCtrl }
-        guard let navigationCtrl = presentedViewCtrl as? UINavigationController else { return topViewController(in: presentedViewCtrl) }
-        return topViewController(in: navigationCtrl.viewControllers.last!)
+        private func topViewController(in viewCtrl: UIViewController) -> UIViewController {
+            guard let presentedViewCtrl = viewCtrl.presentedViewController else { return viewCtrl }
+            guard let navigationCtrl = presentedViewCtrl as? UINavigationController else { return topViewController(in: presentedViewCtrl) }
+            return topViewController(in: navigationCtrl.viewControllers.last!)
+        }
     }
-}
+#endif
